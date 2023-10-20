d91967de51b8ee18d6988aea15d48abba299f1fc
fatinanjum07 <113332116+fatinanjum07@users.noreply.github.com>
2023-10-20 12:39:53 +0600

    Add files via upload

---
+++ Roll_dice.py
@@ -0,0 +1,20 @@
+import random
+
+response = "y"
+
+while response == "y":
+
+    no random.randit(1,6)
+
+    if n0 == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")    
+    if no == 2:
+        print("[-----]")
+        print("[ 0   ]")   
+        print("[     ]")
+        print("[   0 ]")
+    
